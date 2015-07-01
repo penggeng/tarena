6f1ec4dd394634febcf811d92e7ff5788bea0d28
tarena <tarena@tarenadeiMac-9.local>
2015-06-30 23:27:31 -0700

    add

---
+++ repons/repons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  repons
+//
+//  Created by tarena on 15/6/30.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ repons/repons/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  repons
+//
+//  Created by tarena on 15/6/30.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
